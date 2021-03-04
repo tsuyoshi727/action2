ca57bce3eccb58cd2b87667aa8fb41bfd3f28eab
ip <ip@gmail.com>
2021-03-04 20:34:48 +0800

    Merge

---
+++ Node/bububaoCOOKIE.js
@@ -0,0 +1,14 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let bububaotokenVal = `30B933FF7C8E188DBB0E19135526466G1611113251
+F4D7150D8B0EFA866BE963FE7526533G1611119633
+AD26184D3761F44E3AB1A6DA4525199G1611115964`
+
+
+
+let bububaocookie = {
+    bububaotokenVal: bububaotokenVal,
+
+}
+
+module.exports = bububaocookie
