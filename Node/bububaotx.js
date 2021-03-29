35eee3bab2938605ac1605491406cdcec7c88a26
ip <ip@gmail.com>
2021-03-29 15:57:58 +0800

    Merge

--- Node/bububaotx.js
+++ Node/bububaotx.js
@@ -1,8 +1,8 @@
 /* ziye 
-github地址 https://github.com/6Svip120apk69
+github地址 https://github.com/ziye888
 TG频道地址  https://t.me/ziyescript
 TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
-boxjs链接  https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/ziye.boxjs.json
+boxjs链接  https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye.boxjs.json
 
 转载请备注个名字，谢谢
 
@@ -23,6 +23,8 @@ boxjs链接  https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThx
 3.8 替换为循环获取ck
 3.13 修复0.3提现
 3.23 设置CASH为1000以上时则在23.59分执行1秒的循环提现，以此类推
+3.25 替换为await形式
+3.29 优化50提现 设置CASH为3000，则在23.59分执行3秒的循环-以此类推，且在0点后执行1次提现，请提前手动运行或者设置好定时
 
 ⚠️ 时间设置    0,30 0-23 * * *    每天 35次以上就行   
 
@@ -52,17 +54,17 @@ hostname=bububao.duoshoutuan.com,
 
 ############## 圈x
 #步步宝获取TOKEN
-https:\/\/bububao\.duoshoutuan\.com\/user\/* url script-request-header https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/bububao.js
+https:\/\/bububao\.duoshoutuan\.com\/user\/* url script-request-header https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/bububao.js
 
 ############## loon
 #步步宝获取TOKEN
-http-response https:\/\/bububao\.duoshoutuan\.com\/user\/* script-path=https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/bububao.js, requires-body=1,max-size=0, tag=步步宝获取TOKEN
+http-response https:\/\/bububao\.duoshoutuan\.com\/user\/* script-path=https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/bububao.js, requires-body=1,max-size=0, tag=步步宝获取TOKEN
 
 ############## surge
 #步步宝获取TOKEN
-步步宝获取TOKEN = type=http-response,pattern=https:\/\/bububao\.duoshoutuan\.com\/user\/*,script-path=https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/bububao.js
+步步宝获取TOKEN = type=http-response,pattern=https:\/\/bububao\.duoshoutuan\.com\/user\/*,script-path=https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/bububao.js
 */
-GXRZ = '3.23 设置CASH为1000则在23.59分执行1秒的50元循环提现，以此类推'
+GXRZ = '3.29 优化50提现 设置CASH为3000，则在23.59分执行3秒的循环-以此类推，且在0点后执行1次提现，请提前手动运行或者设置好定时'
 const $ = Env("步步宝");
 $.idx = ($.idx = ($.getval('bububaoSuffix') || '1') - 1) > 0 ? ($.idx + 1 + '') : ''; // 账号扩展字符
 const notify = $.isNode() ? require("./sendNotify") : ``;
@@ -76,8 +78,8 @@ const bububaotokenArr = [];
 let bububaotokenVal = ``;
 let middlebububaoTOKEN = [];
 if ($.isNode()) {
-    // 没有设置 FL_DHCASH 则默认为 0 不兑换
-    CASH = process.env.BBB_CASH || 2000;
+    // 没有设置 BBB_DHCASH 则默认为 0 不兑换
+    CASH = process.env.BBB_CASH || 3000;
 }
 if ($.isNode() && process.env.BBB_bububaoTOKEN) {
     COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
@@ -329,17 +331,19 @@ async function all() {
         };
         O = (`${$.name + (i + 1)}🔔`);
 
-
-        if (CASH >= 1000&&nowTimes.getHours() === 23&&nowTimes.getMinutes() == 59) {
+// 
+        if (CASH >= 1000 && nowTimes.getHours() === 23 && nowTimes.getMinutes() == 59) {
             A = Date.now()
             B = Date.now() + CASH
-            C= daytime()+86400000
+            C = daytime() + 86400000
+            D = 0
             while (Date.now() <= B) {
-              if (Date.now() >= C&&Date.now() <= C+5) {
-                CASH = 50
-                 tixian()
-             }
-         }
+                if (Date.now() >= C && D < 1) {
+                    CASH = 50
+                    tixian()
+                    D++;
+                } 
+            } 
 
         } else {
             await console.log(`-------------------------\n\n🔔开始运行【${$.name+(i+1)}】`)
